41c6c857cd60ffefc3060012003a56352e508efc
nimurr <nimurnerob404@gmail.com>
2025-06-04 15:36:06 +0600

    update

--- src/component/Main/Sidebar/Sidebar.jsx
+++ src/component/Main/Sidebar/Sidebar.jsx
@@ -17,6 +17,7 @@ import { GrDocumentImage } from "react-icons/gr";
 import { CiCalendar } from "react-icons/ci";
 import { BiDirections } from "react-icons/bi";
 import { FiKey } from "react-icons/fi";
+import { RxCross1 } from "react-icons/rx";
 
 const sidebarItems = [
   {
@@ -142,10 +143,13 @@ const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
 
       {/* Mobile Sidebar */}
       <div
-        className={`fixed top-0 overflow-y-auto left-0 z-40 w-64 h-full bg-[#4b1c2f] shadow-lg transform ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        className={`fixed top-0 overflow-y-auto left-0 z-40 w-[280px] h-full bg-[#4b1c2f] shadow-lg transform ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"
           } transition-transform duration-300 ease-in-out md:hidden`}
       >
-        <div className="flex flex-col justify-center items-center pt-5 gap-2 ">
+        <div onClick={toggleSidebar} className="absolute top-0 right-0 p-4">
+          <RxCross1 className="size-6 text-white" />
+        </div>
+        <div className="flex flex-col justify-center items-center pt-10 gap-2 ">
           <img src={logo} alt="logo" className="h-20 mb-5" />
         </div>
         <ul className="flex flex-col gap-3 mt-10">
@@ -155,7 +159,7 @@ const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
               to={item.path}
               onClick={toggleSidebar} // Close sidebar on link click
               className={({ isActive }) =>
-                `w-[70%] mx-auto px-5 py-2 flex items-center gap-3 text-white ${isActive ? "bg-[#4b1c2f] " : ""
+                `w-[80%] mx-auto md:px-5 px-2 py-2 flex items-center gap-3 text-white ${isActive ? "bg-[#4b1c2f] " : ""
                 }`
               }
             >
