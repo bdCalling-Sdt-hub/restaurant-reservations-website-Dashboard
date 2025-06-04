43ad658e273dabaac2138602bcbf8b8a7dc69127
nimurr <nimurnerob404@gmail.com>
2025-06-04 15:13:37 +0600

    update

--- src/component/Main/Dashboard/Status.jsx
+++ src/component/Main/Dashboard/Status.jsx
@@ -10,12 +10,12 @@ const Status = () => {
         <p className=" text-sm flex items-center gap-2">+11.01% <IoIosTrendingUp /></p>
       </div>
       <div className="bg-[#4b1c2fd3] text-white p-4 rounded-lg shadow-md">
-        <h3 className="text-2xl font-semibold ">Total User</h3>
+        <h3 className="text-2xl font-semibold ">Total Bookings</h3>
         <p className="text-5xl font-bold my-2">1,000</p>
         <p className=" text-sm flex items-center gap-2">+11.01% <IoIosTrendingUp /></p>
       </div>
       <div className="bg-[#4b1c2fd3] text-white p-4 rounded-lg shadow-md">
-        <h3 className="text-2xl font-semibold ">Total User</h3>
+        <h3 className="text-2xl font-semibold ">Total Biddings</h3>
         <p className="text-5xl font-bold my-2">1,000</p>
         <p className=" text-sm flex items-center gap-2">+11.01% <IoIosTrendingUp /></p>
       </div>
